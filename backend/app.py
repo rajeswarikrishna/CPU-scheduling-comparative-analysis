3be3e6cdcbe8fce9b86f7fe456495846b2f5b873
rajeswari <rajeswarikrishna2409@gmail.ci>
2025-10-31 16:19:36 +0530

    inital commit

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from scheduler import multilevel_queue, multilevel_feedback, hrrn, fair_share
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -31,3 +32,5 @@ def schedule():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
